8b7481f85b1f4b5a447ae3e59064cff574570dc8
zurabza <154089953+zurabza@users.noreply.github.com>
2023-12-20 16:17:27 +0400

    ➕ Add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar"
 import Home from "./layout/home"
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Home />
     </div>
   )
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import REDBERRY from "../../assets/REDBERRY.png";
+
+function Navbar() {
+  return (
+    <div>
+      <img src={REDBERRY} alt="Redberry logo" />
+
+      <button>შესვლა</button>
+    </div>
+  );
+}
+
+export default Navbar;
