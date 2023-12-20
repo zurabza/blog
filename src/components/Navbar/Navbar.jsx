bb563cf16727a53bd7bd95279fb03e0a92a4bc1b
zurabza <154089953+zurabza@users.noreply.github.com>
2023-12-20 16:36:23 +0400

    🎨 Style Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
+import styles from "./Navbar.module.css";
 import REDBERRY from "../../assets/REDBERRY.png";
 
 function Navbar() {
   return (
-    <div>
+    <div className={styles.container}>
       <img src={REDBERRY} alt="Redberry logo" />
 
       <button>შესვლა</button>
