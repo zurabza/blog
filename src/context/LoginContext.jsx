b47dfc0a8c14b826bc61d96a3e76d72f9eef82e6
zurabza <154089953+zurabza@users.noreply.github.com>
2023-12-21 13:53:22 +0400

    Persist isLoggedIn state on page reload

--- src/context/LoginContext.jsx
+++ src/context/LoginContext.jsx
@@ -1,13 +1,20 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
 
 const LoginContext = createContext();
 
 export const LoginProvider = ({ children }) => {
-  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isLoggedIn, setLoggedIn] = useState(() => {
+    const storedValue = localStorage.getItem("isLoggedIn");
+    return storedValue ? JSON.parse(storedValue) : false;
+  });
 
   const login = () => {
     setLoggedIn(true);
   };
+
+  useEffect(() => {
+    localStorage.setItem("isLoggedIn", JSON.stringify(isLoggedIn));
+  }, [isLoggedIn]);
 
   return <LoginContext.Provider value={{ isLoggedIn, login }}>{children}</LoginContext.Provider>;
 };
