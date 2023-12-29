56886557b98cf16efd53e4b58e5812340eaefff3
zurabza <154089953+zurabza@users.noreply.github.com>
2023-12-29 18:05:40 +0400

    Add Blogpost component for individual blogpost & /:id route for it

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Home from "./layout/home";
 import AddBlogpost from "./components/AddBlogpost";
+import Blogpost from "./components/Blogpost";
 import NotFound from "./components/NotFound";
 
 import { Routes, Route } from "react-router-dom";
@@ -18,6 +19,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/add" element={isLoggedIn ? <AddBlogpost /> : <NotFound content="ავტორიზაცია სავალდებულოა" />} />
+        <Route path="/:id" element={<Blogpost />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BlogpostProvider>
